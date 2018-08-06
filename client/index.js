a64bba80cbc17962985292eac20fb5ed98099f39
Maria Castro <mariacastro87@gmail.com>
2018-08-05 23:36:28 -0400

    Finish setting Jest config for client and server

--- client/index.js
+++ client/index.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { PropTypes } from 'react';
 import ReactDOM from "react-dom";
 
-const Index = () => {
-  return <div>Hello React Refresh!</div>;
-};
+const propTypes = {};
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+const defaultProps = {};
+
+class Foo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="foo">
+        Bar
+      </div>
+    );
+  }
+}
+
+Foo.propTypes = propTypes;
+Foo.defaultProps = defaultProps;
+
+export default Foo;
+
+//ReactDOM.render(<Foo />, document.getElementById("index"));
