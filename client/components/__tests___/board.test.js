240d5034bf5810bf0a69c74da90121a91a62b13d
Maria Castro <mariacastro87@gmail.com>
2018-08-27 19:13:28 -0300

    Finish initial Board Component (creation and name change)

--- client/components/__tests___/board.test.js
+++ client/components/__tests___/board.test.js
@@ -6,13 +6,39 @@ describe("Board Component", () => {
     expect(shallow(<Board data={{}} />)).toBeTruthy;
   });
 
-  it("should render with Board data", () => {
+  it("Should set initial state in component", () => {
+    const boardComponent = shallow(<Board data={{}} />);
+
+    expect(boardComponent.state("name")).toEqual("Untitled Board");
+  });
+
+  it("Should set initial state in component given a prop", () => {
     const dataProps = {
-      name: "Test Board"
+      name: "Board Test"
     };
 
     const boardComponent = shallow(<Board data={dataProps} />);
 
-    expect(boardComponent.contains(<div>Test Board</div>)).toBe(true);
+    expect(boardComponent.state("name")).toEqual("Board Test");
+  });
+
+  it("Should render with Board data", () => {
+    const dataProps = {
+      name: "Board Name"
+    };
+
+    const boardComponent = shallow(<Board data={dataProps} />);
+    expect(boardComponent.contains(<div>Board Name</div>)).toBe(true);
+  });
+
+  it("Should change Board name", () => {
+    const dataProps = {
+      name: "Board Name"
+    };
+
+    const boardComponent = shallow(<Board data={dataProps} />);
+    boardComponent.instance().changeName("New name");
+
+    expect(boardComponent.state("name")).toEqual("New name");
   });
 });
--- client/components/board.js
+++ client/components/board.js
@@ -2,7 +2,17 @@ import React from "react";
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: props.data.name ? props.data.name : "Untitled Board"
+    };
   }
+
+  changeName(newName) {
+    this.setState({
+      name: newName
+    });
+  }
+
   render() {
     return <div>{this.props.data.name}</div>;
   }
