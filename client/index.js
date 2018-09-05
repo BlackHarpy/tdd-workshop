1ab13f363e9ed85e0af2b2e5cb032ddb14ee448e
Maria Castro <mariacastro87@gmail.com>
2018-09-05 18:58:14 -0300

    Set project start

--- client/index.js
+++ client/index.js
@@ -1,39 +1,24 @@
-import React, { PropTypes } from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
 
-const propTypes = {};
-
-const defaultProps = {};
-
-class Foo extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return (
-      <div className="foo">
-        Bar
-      </div>
-    );
+    return <div className="foo">Bar</div>;
   }
 }
 
-Foo.propTypes = propTypes;
-Foo.defaultProps = defaultProps;
-
-
-fetch('http://localhost:8080/ping')
-  .then(function (resp) {
+fetch("http://localhost:8080/ping")
+  .then(function(resp) {
     resp.text().then(data => {
       console.log(data);
     });
   })
-  .catch(function (error) {
+  .catch(function(error) {
     // If there is any error you will catch them here
   });
 
-
-export default Foo;
-
-//ReactDOM.render(<Foo />, document.getElementById("index"));
+ReactDOM.render(<App />, document.getElementById("index"));
