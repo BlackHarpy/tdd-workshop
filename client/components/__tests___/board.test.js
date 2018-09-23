1cfe75adc976c44a107041004dbe7783addc985d
Maria Castro <mariacastro87@gmail.com>
2018-09-23 00:55:04 -0300

    Board full functionalities

--- client/components/__tests___/board.test.js
+++ client/components/__tests___/board.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Board from "../board";
+import List from "../list";
 
 describe("Board Component", () => {
   it("Should render without throwing an error", () => {
@@ -29,7 +30,7 @@ describe("Board Component", () => {
     };
 
     const boardComponent = shallow(<Board data={dataProps} />);
-    expect(boardComponent.contains(<div>Board Name</div>)).toBe(true);
+    expect(boardComponent.contains(<div>Board Name</div>)).toEqual(true);
   });
 
   it("Should change Board name", () => {
@@ -41,5 +42,152 @@ describe("Board Component", () => {
     boardComponent.instance().changeName("New name");
 
     expect(boardComponent.state("name")).toEqual("New name");
+  });
+
+  it("Should render in order", () => {
+    const dataProps = {
+      name: "Board Name"
+    };
+
+    const boardComponent = shallow(<Board data={dataProps} />);
+    boardComponent.setState({
+      lists: [{ position: 0 }, { position: 2 }, { position: 1 }]
+    });
+
+    boardComponent.update();
+    const lists = boardComponent.find(List);
+
+    expect(lists.at(0).props().data.position).toEqual(0);
+    expect(lists.at(1).props().data.position).toEqual(1);
+    expect(lists.at(2).props().data.position).toEqual(2);
+  });
+
+  it("Should create a new list", () => {
+    const dataProps = {
+      name: "Board Name"
+    };
+
+    const boardComponent = shallow(<Board data={dataProps} />);
+    boardComponent.instance().addList();
+    boardComponent.update();
+    expect(
+      boardComponent.contains(
+        <List data={{ id: 0, position: 0, name: "List 1" }} />
+      )
+    ).toEqual(true);
+  });
+
+  it("Should create two lists", () => {
+    const dataProps = {
+      name: "Board Name"
+    };
+
+    const boardComponent = shallow(<Board data={dataProps} />);
+    boardComponent.instance().addList();
+    boardComponent.instance().addList();
+    boardComponent.update();
+
+    expect(boardComponent.find(List)).toHaveLength(2);
+  });
+
+  it("Should delete a list", () => {
+    const dataProps = {
+      name: "Board Name"
+    };
+
+    const boardComponent = shallow(<Board data={dataProps} />);
+    boardComponent.setState({
+      lists: [{ id: 0 }, { id: 1 }, { id: 2 }]
+    });
+
+    boardComponent.instance().deleteList(1);
+
+    boardComponent.update();
+    const lists = boardComponent.find(List);
+
+    expect(lists).toHaveLength(2);
+    expect(lists.at(0).props().data.id).toEqual(0);
+    expect(lists.at(1).props().data.id).toEqual(2);
+  });
+
+  it("Should change list name", () => {
+    const dataProps = {
+      name: "Board Name"
+    };
+
+    const boardComponent = shallow(<Board data={dataProps} />);
+    boardComponent.setState({
+      lists: [{ id: 0, position: 0, name: "List 1" }]
+    });
+
+    boardComponent.instance().changeListName(0, "List Test");
+
+    boardComponent.update();
+
+    expect(boardComponent.state("lists")[0].name).toEqual("List Test");
+    expect(
+      boardComponent
+        .find(List)
+        .at(0)
+        .props().data.name
+    ).toEqual("List Test");
+  });
+
+  it("Should change list position", () => {
+    const dataProps = {
+      name: "Board Name"
+    };
+
+    const boardComponent = shallow(<Board data={dataProps} />);
+    boardComponent.setState({
+      lists: [
+        { id: 0, position: 0, name: "List 1" },
+        { id: 1, position: 1, name: "List 2" },
+        { id: 2, position: 2, name: "List 3" }
+      ]
+    });
+
+    boardComponent.instance().changePosition(0, 2);
+    boardComponent.update();
+
+    const expectedListState = [
+      { id: 0, position: 2, name: "List 1" },
+      { id: 1, position: 0, name: "List 2" },
+      { id: 2, position: 1, name: "List 3" }
+    ];
+
+    expect(boardComponent.state("lists").sort((a, b) => a.id - b.id)).toEqual(
+      expectedListState
+    );
+  });
+
+  it("Should change list position", () => {
+    const dataProps = {
+      name: "Board Name"
+    };
+
+    const boardComponent = shallow(<Board data={dataProps} />);
+    boardComponent.setState({
+      lists: [
+        { id: 0, position: 0, name: "List 1" },
+        { id: 1, position: 1, name: "List 2" },
+        { id: 2, position: 2, name: "List 3" },
+        { id: 3, position: 3, name: "List 4" }
+      ]
+    });
+
+    boardComponent.instance().changePosition(3, 2);
+    boardComponent.update();
+
+    const expectedListState = [
+      { id: 0, position: 0, name: "List 1" },
+      { id: 1, position: 1, name: "List 2" },
+      { id: 2, position: 3, name: "List 3" },
+      { id: 3, position: 2, name: "List 4" }
+    ];
+
+    expect(boardComponent.state("lists").sort((a, b) => a.id - b.id)).toEqual(
+      expectedListState
+    );
   });
 });
--- client/components/board.js
+++ client/components/board.js
@@ -7,7 +7,8 @@ export default class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: props.data.name ? props.data.name : "Untitled Board"
+      name: props.data.name ? props.data.name : "Untitled Board",
+      lists: []
     };
   }
 
@@ -17,14 +18,76 @@ export default class Board extends React.Component {
     });
   }
 
+  addList() {
+    this.setState({
+      lists: [
+        ...this.state.lists,
+        {
+          id: this.state.lists.length,
+          position: this.state.lists.length,
+          name: `List ${this.state.lists.length + 1}`
+        }
+      ]
+    });
+  }
+
+  deleteList(index) {
+    this.setState({
+      lists: this.state.lists.filter((list, id) => index !== id)
+    });
+  }
+
+  changeListName(id, newName) {
+    this.setState({
+      lists: this.state.lists.map(list => {
+        if (list.id === id) {
+          list.name = newName;
+        }
+        return list;
+      })
+    });
+  }
+
+  moveElements(lists, id, newPosition) {
+    const element = lists.find(list => list.id === id);
+    const oldPosition = element.position;
+
+    return lists.map(list => {
+      if (list.id === id) {
+        list.position = newPosition;
+      } else {
+        if (newPosition > oldPosition) {
+          if (list.position <= newPosition) {
+            list.position--;
+          }
+        } else {
+          if (list.position >= newPosition) {
+            list.position++;
+          }
+        }
+      }
+      return list;
+    });
+  }
+
+  changePosition(id, newPosition) {
+    this.setState({
+      lists: this.moveElements(this.state.lists, id, newPosition)
+    });
+  }
+
   render() {
     return (
       <div style={styles.board}>
         <div>{this.props.data.name}</div>
-
         <div style={styles.boardLists}>
-          <List data={{}} />
-          <List data={{}} />
+          {this.state.lists
+            .sort((a, b) => {
+              return a.position - b.position;
+            })
+            .map((listData, index) => (
+              <List key={index} data={listData} />
+            ))}
         </div>
       </div>
     );
